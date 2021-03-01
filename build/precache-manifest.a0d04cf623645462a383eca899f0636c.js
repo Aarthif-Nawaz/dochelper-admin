54eeb6a8efe80dd902bc795ba6fc1ec0e0c8499b
Aarthif <aarthif@wso2.com>
2021-03-02 00:27:18 +0530

    Added Build

---
+++ build/precache-manifest.a0d04cf623645462a383eca899f0636c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b5f2da1f4b75a48fd0ca51e78315b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ddf03fdc43b15a9c5fe",
+    "url": "/static/css/2.c00ef52c.chunk.css"
+  },
+  {
+    "revision": "e6fc8429c8eb042b8cfc",
+    "url": "/static/css/main.82868f58.chunk.css"
+  },
+  {
+    "revision": "9ddf03fdc43b15a9c5fe",
+    "url": "/static/js/2.e99f0d7a.chunk.js"
+  },
+  {
+    "revision": "590b90636d6f33523c1feaef8074011d",
+    "url": "/static/js/2.e99f0d7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fc8429c8eb042b8cfc",
+    "url": "/static/js/main.5bbe2012.chunk.js"
+  },
+  {
+    "revision": "5e65153bf6807e2aa5d8",
+    "url": "/static/js/runtime-main.10034424.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
